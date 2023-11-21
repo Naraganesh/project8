e07e0f29fb5695e650d9b821a98d4ab61cf254bd
Naraganesh <naraganesh143@gmail.com>
2023-11-21 19:32:13 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ganesh(request):
+    return render(request,'ganesh.html')
+def chinna(request):
+    return render(request,'chinna.html')
+def ganesh2(request):
+    return render(request,'ganesh2.html')
